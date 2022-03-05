a7af0742d1ff5ae00a177ccff466974d7116c95a
Roel Noback <roel@noback.com>
2022-03-05 13:15:10 +0100

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
     refinedMoonstone: 4
 }
 
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4
+}
+
 console.log("elvenShieldRecipe ", elvenShieldRecipe);
+console.log("elvenGauntletsRecipe ", elvenGauntletsRecipe);
