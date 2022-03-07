3ced8c0451b7103a0867c513623398d973bfde93
Roel Noback <roel@noback.com>
2022-03-07 16:45:29 +0100

    Add Image support

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import Recipes from "./Recipes"
+import Recipes from "./Recipes";
+import wear from "../images/wealgowear.jpg";
+import world from "../images/worldLow.svg";
 
 const App = () => {
     return (
         <div>
-            <section className="hero"></section>
+            <section className="hero">
+
+            </section>
             <main>
                 <section>
                     <h1>Oh hai React</h1>
                 </section>
+                <img src={wear} alt={wear} width="250" />
+                <img src={world} alt={world} height="250" width="450" />
             </main>
             <Recipes />
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,31 @@ if (process.env.NODE_ENV === "production" ) {
 
 module.exports = {
     mode: mode,
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset", // "asset/resource" => loads all files form folder, "assets" puts small files into js files as a BASE64 type
+                /*parser: {
+                    dataUrlCondition: {
+                        maxSize: 30 * 1024
+                    }
+                }*/
+            },
+            {
                 test: /\.(s[ac]|c)ss$/i,
-                use:[MiniCssExtractPlugin.loader, "css-loader","postcss-loader", "sass-loader"]
+                use:[
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: "" }
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             },
             {
                 test: /\.jsx?$/, // Any file that ends on .js
